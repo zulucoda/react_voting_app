9c9ac0da042932fc4bdfc9aad311fa762304f160
Muzikayise Flynn Buthelezi aka zuluCoda <muzi@mfbproject.co.za>
2017-01-27 23:16:37 +0200

    making product dynamic :fire: :fire:

--- app.js
+++ app.js
@@ -2,35 +2,65 @@
 
 
 const ProductList = React.createClass({
+  handleProductUpVote: (productId) => {
+    console.log(productId + ' was up voted.');
+  },
 
   render : function () {
+    const products = Data.map((product) => {
+      return (
+        <div className='ui items'>
+          <Product
+            key={'product-'+product.id}
+            id={product.id}
+            title={product.title}
+            description={product.description}
+            url={product.url}
+            votes={product.votes}
+            submitter_avatar_url={product.submitter_avatar_url}
+            product_image_url={product.product_image_url}
+            onVote={this.handleProductUpVote}
+          />
+        </div>
+      );
+    });
 
     return (
-      <div className='ui items'>
-        <Product />
+      <div className="ui items">
+        {products}
       </div>
     );
-
   }
 
 });
 
 const Product = React.createClass({
 
+  handleUpVote: function() {
+    this.props.onVote(this.props.id);
+  },
+
   render: function () {
     return (
       <div className="item">
         <div className="image">
-          <img src="images/products/image-aqua.png" alt=""/>
+          <img src={this.props.product_image_url} alt=""/>
         </div>
         <div className="middle aligned content">
+          <div className="header">
+            <a onClick={this.handleUpVote}>
+              <i className="large caret up icon">
+              </i>
+              {this.props.votes}
+            </a>
+          </div>
           <div className="description">
-            <a href="">Fort Knight</a>
-            <p>Authentic renaissances actors, delivered in just two weeks.</p>
+            <a href={this.props.url}>{this.props.title}</a>
+            <p>{this.props.description}</p>
           </div>
           <div className="extra">
             <span>Submitted by:</span>
-            <img className="ui avatar image" src="images/avatars/daniel.jpg" alt=""/>
+            <img className="ui avatar image" src={this.props.submitter_avatar_url} alt=""/>
           </div>
         </div>
       </div>
